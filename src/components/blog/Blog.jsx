250a575d38801ffa5e787954ab59c9020b8b579b
abdullokhpro <abdulloxrasulov441@gmail.com>
2024-06-19 18:51:02 +0500

    everthing done

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -2,8 +2,23 @@ import React from "react";
 import { IoTime } from "react-icons/io5";
 
 import "./blog.scss";
+import { BLOG_DATA } from "../../static/data";
 
 const Blog = () => {
+  // BLOG DATA MAP
+  // const blogData = BLOG_DATA?.map((info) => (
+  //   <div key={info.id} className="blog__card">
+  //     <span className="blog__card__time">
+  //       <IoTime className="blog__card__time-icon" />
+  //       {info.time}
+  //     </span>
+
+  //     <h3 className="blog__card__title">{info.title}</h3>
+  //     <p className="blog__card__text">{info.desc}</p>
+  //     <button className="blog__card__btn">Read</button>
+  //   </div>
+  // ));
+
   return (
     <div className="blog">
       <div className="container">
@@ -14,7 +29,7 @@ const Blog = () => {
         <div className="blog__cards">
           <div className="blog__cards__left">
             <div className="blog__cards__top">
-              <div className="blog__card blog__card--top">
+              <div className="blog__card">
                 <span className="blog__card__time">
                   <IoTime className="blog__card__time-icon" />
                   12.09.2021
@@ -31,12 +46,46 @@ const Blog = () => {
               </div>
             </div>
             <div className="blog__cards__bottom">
-              <div className="blogs__card"></div>
-              <div className="blogs__card"></div>
+              <div className="blog__card blog__cards-bottom">
+                <span className="blog__card__time">
+                  <IoTime className="blog__card__time-icon" />
+                  12.09.2021
+                </span>
+
+                <h3 className="blog__card__title">
+                  How to plant spinach correctly in winter
+                </h3>
+                <button className="blog__card__btn">Read</button>
+              </div>
+              <div className="blog__card blog__cards-bottom">
+                <span className="blog__card__time">
+                  <IoTime className="blog__card__time-icon" />
+                  12.09.2021
+                </span>
+
+                <h3 className="blog__card__title">
+                  How to plant spinach correctly in winter
+                </h3>
+                <button className="blog__card__btn">Read</button>
+              </div>
             </div>
           </div>
           <div className="blog__cards__right">
-            <div className="blogs__card"></div>
+            <div className="blog__card">
+              <span className="blog__card__time">
+                <IoTime className="blog__card__time-icon" />
+                12.09.2021
+              </span>
+
+              <h3 className="blog__card__title">
+                How to plant spinach correctly in winter
+              </h3>
+              <p className="blog__card__text">
+                In most areas, successive sowing can be done from early spring
+                until early winter, but more often during hotter months...
+              </p>
+              <button className="blog__card__btn">Read</button>
+            </div>
           </div>
         </div>
       </div>
--- src/static/data.js
+++ src/static/data.js
@@ -1,1 +1,36 @@
-console.log('static');
+console.log("static");
+
+// BLOG DATA IMAGES
+import blogTop from "../assets/images/blog-top.png";
+import blogBottom from "../assets/images/blog-bottom.png";
+import blogRight from "../assets/images/blog-right.png";
+
+// BLOG DATA
+export const BLOG_DATA = [
+  {
+    id: 1,
+    title: "How to plant spinach correctly in winter",
+    time: "12.09.2021",
+    desc: "In most areas, successive sowing can be done from early spring until early winter, but more often during hotter months...",
+    url: blogTop,
+  },
+  {
+    id: 2,
+    title: "How to plant spinach correctly in winter",
+    time: "12.09.2021",
+    desc: "In most areas, successive sowing can be done from early spring until early winter, but more often during hotter months...",
+    url: blogRight,
+  },
+  {
+    id: 3,
+    title: "How to plant spinach correctly in winter",
+    time: "12.09.2021",
+    url: blogBottom,
+  },
+  {
+    id: 4,
+    title: "How to plant spinach correctly in winter",
+    time: "12.09.2021",
+    url: blogBottom,
+  },
+];
